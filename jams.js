43981cbf71c18594cc0b92b71fb4d7541ab379f0
Daniel Brockman <daniel@brockman.se>
2022-06-09 02:37:39 +0200

    Make code a bit more compact

--- jams.js
+++ jams.js
@@ -21,36 +21,29 @@ JAMS.stringify = x => {
 
 JAMS.parse = input => {
   let when = (x, f, g) => x != null ? f(x) : g && g()
-  let make = (x, f) => (f(x), x)
-  let spot = () => (x => `${x.split("\n").length}:${
-    x.replace(/^[.\n]*\n/, "").length + 1
-  }`)(input.substr(0, i)), i = 0
-
+  let make = (x, f) => (f(x), x), id = x => x, i = 0
   let accept = x => (x.lastIndex = i, when(
     x.exec(input), y => (i = x.lastIndex, y[0])
-  )), expect = (x, y) => when(accept(x), x => x, () => {
-    throw new Error([
-      `Expected ${y}, found ${i < input.length ? "\`" + (
-        JSON.stringify(input[i]).replace(/^"|"$/g, "")
-      ) + "'" : "eof"} (${spot()})`,
-    ].join(" "))
-  })
-
-  let parse = () => {
-    expect(/^|(?<=[\[\{])|\s+/y, `space`); if (accept(/"/y)) {
-      let x = expect(/([^"\r\n\\]|\\([bfnrt\\"]|u[0-9a-fA-F]{4}))*/yu)
-      return expect(/"/y, `quote`), JSON.parse(`"${x}"`)
-    } else if (accept(/\[/y)) return make([], xs => {
+  )), expect = (x, y=`\`${x.source}'`) => when(accept(x), id, () => {
+    throw new Error(`Expected ${y}, found ${i < input.length ? "\`" + (
+      JSON.stringify(input[i]).replace(/^"|"$/g, "")
+    ) + "'" : "eof"} (${spot()})`)
+  }), parse = () => (expect(/^|(?<=[\[\{])|\s+/y, `space`), (
+    accept(/"/y) ? JSON.parse(`"${make(expect(
+      /([^"\r\n\\]|\\([bfnrt\\"]|u[0-9a-fA-F]{4}))*/yu
+    ), () => expect(/"/y))}"`) : accept(/\[/y) ? make([], xs => {
       while (!accept(/\s*\]/y)) xs.push(parse())
-    }); else if (accept(/\{/y)) return make({}, x => {
+    }) : accept(/\{/y) ? make({}, x => {
       while (!accept(/\s*\}/y)) {
         let [k, i0, v] = [parse(), i, parse()]
         if (k in x) throw new Error(`Duplicate key \`${k}' (${
           i = i0 - k.length, spot()
-        })`); else x[k] = v
+        })`); x[k] = v
       }
-    }); else return expect(/[^\s\[\]{}"\\]+/yu, `value`)
-  }
+    }) : expect(/[^\s\[\]{}"\\]+/yu, `value`)
+  )), spot = () => (x => `${x.split("\n").length}:${
+    x.replace(/^[.\n]*\n/, "").length + 1
+  }`)(input.substr(0, i))
 
   return make(parse(), () => accept(/\s*/y), expect(/$/y, `eof`))
 }
